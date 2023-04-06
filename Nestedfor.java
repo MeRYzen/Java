647b90f84708e3809b1bcbadaf171002c48b982b
Meryzennn <85497760+MeRYzen@users.noreply.github.com>
2023-04-06 08:39:40 +0700

    Add files via upload

---
+++ Nestedfor.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.nestedfor;
+
+/**
+ *
+ * @author ryzen
+ */
+public class Nestedfor {
+
+    public static void main(String[] args) {
+        int a, b;
+        for(a  =  1;  a <=15; a++)
+                {
+                    System.out.println();
+                for(b =  a; b <= 15; b++ )
+                {
+                    System.out.print (a);
+                }
+                }
+        System.out.println("/n");
+    }
+}
